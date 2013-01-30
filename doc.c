be325cf6ed1fd1ce038ae03f1797e91ab82c5a03
Egregious <egregious@iscabbs.com>
2013-01-30 11:09:51 -0800

    force telnet into character mode

--- doc.c
+++ doc.c
@@ -20,7 +20,9 @@ char    bueller = 0;
   setvbuf(stdout, (char *)stdoutbuf, _IOFBF, STDOUTBUFSIZ);
 
   init_system();
-
+  /* magic to set telnet into character mode */
+  /* IAC  DO LINEMODE, IAC WILL ECHO */
+  write(1,"\377\375\042\377\373\001",6);
   reserve_slot();
   do_login();
 
